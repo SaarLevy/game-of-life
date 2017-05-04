58b31cc8c6180b5fa6f5893eba5306d294992677
Saar Levy <saar.levy@gmail.com>
2017-05-04 21:56:02 +0300

    Changed code structure, some bugs to fix

--- js/cell.js
+++ js/cell.js
@@ -7,39 +7,45 @@ function Cell(i, j){
     this.neighbours = [];
 }
 
-//TODO: find a way to make this function neater
-Cell.prototype.neighbours = function(grid){
 
-    const top     = this.j-1;
-    const right   = this.i+1;
-    const bottom  = this.j+1;
-    const left    = this.i-1;
+Cell.prototype.countNeighbours = function(grid){
+    const top     = this.j - 1;
+    const right   = this.i + 1;
+    const bottom  = this.j + 1;
+    const left    = this.i - 1;
     const middlei = this.i;
     const middlej = this.j;
 
-    if(grid[left][top] != undefined && grid[left][top]){ this.neighbours.push(grid[left][top]);} //Left top
-    if(grid[middlei][top] != undefined && grid[middlei][top]){ this.neighbours.push(grid[middlei][top]);} //Middlei top
-    if(grid[right][top] != undefined && grid[right][top]){ this.neighbours.push(grid[right][top]);} //Right top
-    if(grid[left][middlej] != undefined && grid[left][middlej]){ this.neighbours.push(grid[left][middlej]);} //Left middlej
-    if(grid[right][middlej] != undefined && grid[right][middlej]){ this.neighbours.push(grid[right][middlej]);} //Right middlej
-    if(grid[left][bottom] != undefined && grid[left][bottom]){ this.neighbours.push(grid[left][bottom]);} //Left bottom
-    if(grid[middlei][bottom] != undefined && grid[middlei][bottom]){ this.neighbours.push(grid[middlei][bottom]);} //Middlei bottom
-    if(grid[right][bottom] != undefined && grid[right][bottom]){ this.neighbours.push(grid[right][bottom]);} //Right bottom
-    
+    //TODO
+    if(grid[left][top] > -1 && grid[left][top]){ this.neighbours.push(grid[left][top]);} //Left top
+    if(grid[middlei][top] > -1 && grid[middlei][top]){ this.neighbours.push(grid[middlei][top]);} //Middlei top
+    if(grid[right][top] > -1 && grid[right][top]){ this.neighbours.push(grid[right][top]);} //Right top
+    if(grid[left][middlej] > -1 && grid[left][middlej]){ this.neighbours.push(grid[left][middlej]);} //Left middlej
+    if(grid[right][middlej] > -1 && grid[right][middlej]){ this.neighbours.push(grid[right][middlej]);} //Right middlej
+    if(grid[left][bottom] > -1 && grid[left][bottom]){ this.neighbours.push(grid[left][bottom]);} //Left bottom
+    if(grid[middlei][bottom] > -1 && grid[middlei][bottom]){ this.neighbours.push(grid[middlei][bottom]);} //Middlei bottom
+    if(grid[right][bottom] > -1 && grid[right][bottom]){ this.neighbours.push(grid[right][bottom]);} //Right bottom
 }
 
-Cell.prototype.update = function(){
-    const liveNeighbours = this.neighbours.length;
-    if(this.alive && liveNeighbours < 2) {this.survives = false;}
-    if(this.alive && (liveNeighbours === 2 || liveNeighbours === 3)) {this.survives = true;}
-    if(this.alive && liveNeighbours > 3){this.survives = false;}
-    if(!this.alive && liveNeighbours === 3){this.survives = true;}
+Cell.prototype.update = function(grid){
+
+    
+    // //This is where the magic happens:
+    // if(this.alive && this.neighbours.length < 2) {this.survives = false;}
+    // if(this.alive && (this.neighbours.length === 2 || this.neighbours.length === 3)) {this.survives = true;}
+    // if(this.alive && this.neighbours.length > 3){this.survives = false;}
+    // if(!this.alive && this.neighbours.length === 3){this.survives = true;}
 }
 
 Cell.prototype.render = function(size) {
-    
-    stroke(255);
-    fill(255, 0, 50);
-    rect(this.i * s, this.j*s, s, s);
 
+    if(this.alive){
+        stroke(255);
+        fill(255, 0, 50);
+    } else {
+        stroke(255);
+        fill(180);
+    }
+    rect(this.i * s, this.j*s, s, s);
+    
 } 
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 const h = 800; // Height
 const w = 800; // Width
-const s = 40;  // Size of squares
+const s = 20;  // Size of squares
 const rows = Math.floor(h / s);
 const cols = Math.floor(w / s);
 let cells = new Array(cols);
@@ -9,8 +9,7 @@ let cells = new Array(cols);
 
 
 function setup() {
-    createCanvas(800, 800);
-    background(50);
+    createCanvas(w, h);
     for(let i = 0; i < cols; i++){
         cells[i] = new Array(rows);
         for(let j = 0; j < rows; j++){
@@ -23,7 +22,16 @@ function setup() {
 function draw() {
     for(i = 0; i < cols; i++){
         for(j = 0; j < rows; j++){
+            cells[i][j].countNeighbours(cells);
+            cells[i][j].update();
             cells[i][j].render(s);
         }
     }
+}
+
+function mouseDragged(){
+    const x = floor(map(mouseX, 0, width, 0, cols));
+    const y = floor(map(mouseY, 0, height, 0, rows));
+
+    cells[x][y].alive = true;
 }
